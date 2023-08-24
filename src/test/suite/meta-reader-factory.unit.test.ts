f569c615e81eea088c968eb0d5e51da7501ba1ef
Siwar GAROUACHI <s.garouachi@sap.com>
2023-08-24 13:16:55 +0200

    test(metareaderfactory): add UTs

---
+++ src/test/suite/meta-reader-factory.unit.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { describe, it } from 'mocha';
+import MetaReaderFactory from '../../lib/meta-reader-factory';
+
+describe('MetaReaderFactory - Unit Tests', function () {
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    describe('getExtensionName - Unit Tests', function () {
+        it('Should return extension name', async function () {
+            const expected = 'vs-code-extension-for-project-credential-digger';
+            const result = MetaReaderFactory.getInstance().getExtensionName();
+            expect(result).to.be.eql(expected);
+        });
+    });
+
+    describe('getExtensionScanCommand - Unit Tests', function () {
+        it('Should return scan command name', async function () {
+            const expected =
+                'vs-code-extension-for-project-credential-digger.credentialDiggerScan';
+            const result =
+                MetaReaderFactory.getInstance().getExtensionScanCommand();
+            expect(result).to.be.eql(expected);
+        });
+    });
+
+    describe('getExtensionAddRulesCommand - Unit Tests', function () {
+        it('Should return addRules command name', async function () {
+            const expected =
+                'vs-code-extension-for-project-credential-digger.credentialDiggerAddRules';
+            const result =
+                MetaReaderFactory.getInstance().getExtensionAddRulesCommand();
+            expect(result).to.be.eql(expected);
+        });
+    });
+});
